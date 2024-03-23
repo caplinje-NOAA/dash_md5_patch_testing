2bbc703997468a677ec1c17c2a090e19f269c128
caplinje-NOAA <james.caplinger@NOAA.gov>
2024-03-23 10:07:13 -0400

    register callback

--- main.py
+++ main.py
@@ -8,15 +8,29 @@ Blank dash project for testing hash.md5 swap with hash.sha256
 
 """
 
-from dash import Dash, html, dcc
+from dash import Dash, dcc, html, Input, Output, callback
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.Div(children='Hello World',id='testdiv'),
-    dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'], 'Montréal')
-    
+    html.H6("Change the value in the text box to see callbacks in action!"),
+    html.Div([
+        "Input: ",
+        dcc.Input(id='my-input', value='initial value', type='text')
+    ]),
+    html.Br(),
+    html.Div(id='my-output'),
+
 ])
+
+
+@callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+def update_output_div(input_value):
+    return f'Output: {input_value}'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
